d61aa2c720924ca1da9a4cd7ae91a796d41ba64a
van-sprundel <ramonvansprundel@gmail.com>
2026-01-09 19:55:49 +0100

    mv to std::hint::black_box

--- check-buddy/benches/generate_moves.rs
+++ check-buddy/benches/generate_moves.rs
@@ -1,6 +1,7 @@
 use check_buddy::piece_type::PieceType;
 use check_buddy::BoardMap;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 
 fn generate_moves(c: &mut Criterion) {
     let board = BoardMap::starting();
